e191a13662f1aef62e632b4f362ebba1c7766736
Dmitry Savchenkov <dimychmen@yandex.ru>
2022-08-03 22:33:03 +0300

    первое задание

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+// Задание 1
+    int clientOs = 1;
+    if (clientOs == 0) {
+        System.out.println("Установите версию приложения для IOS по ссылке");
+    } else if (clientOs == 1) {
+        System.out.println("Установите версию приложения для Android по ссылке");
+    } else {
+        System.out.println("Под такую ОС нет приложения");
+    }}}
